ffc53c3fe998e203909a495051ab1c4fca1de21a
Fengrui0 <myangfengrui@gmail.com>
2025-06-04 14:00:01 -0400

    finshed basic react settings, added meta label

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ little-lemon/src/Main.js
@@ -0,0 +1,9 @@
+function Main(){
+    return(
+        <main>
+            <h1>Welcome to Little Lemon</h1>
+            <p>Enjoy fresh Mediterranean flavors in the heart of your city.</p>
+        </main>
+    )
+}
+export default Main;
